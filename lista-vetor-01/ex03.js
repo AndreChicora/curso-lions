0dec098b0967a10f7e40cf66e22fdadaf818093d
andre luiz <andrechicora844@email.com>
2026-03-25 15:29:46 -0300

    ex04 lista de vetor

--- lista-vetor-01/ex03.js
+++
@@ -1,5 +0,0 @@
-let nums = [ 2, 8, 4 , 9, 10, 3];
-let soma = 0;
-for(let i = 2; i < nums.length; i++){
-    soma = soma
-}
---
+++ lista-vetor-01/ex04.js
@@ -0,0 +1,7 @@
+let nums = [ 2, 8, 4 , 9, 10, 3];
+let soma = 0;
+for(let i = 0; i < nums.length; i++){
+    soma = soma + nums[i];
+}
+let media = soma/nums.length
+console.log(media)
