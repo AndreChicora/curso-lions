b5fd2bc931757f9266890f9175d5c4a7c7231ba8
andre luiz <andrechicora844@email.com>
2026-03-26 13:48:35 -0300

    ex03 lista vetor

--- lista-vetor-01/ex03.js
+++ lista-vetor-01/ex03.js
@@ -2,6 +2,7 @@ let nums = [1, 2, 3,4 ,5];
 let soma = 0;
 
 for(let i = 0; i < nums.length; i++){
+    b[i] = a[i] * 2
     console.log(soma = nums [i] + soma);
 }
 console.log("a soma é: ", soma)
---
+++ lista-vetor-01/ex05.js
@@ -0,0 +1,3 @@
+let nums = [10, 25, 7, 89, 32, 14];
+
+let maior = nums[]
