2857e237e3b2bf825b6204bb30173c7ca1789de7
andre luiz <andrechicora844@email.com>
2026-03-26 14:00:09 -0300

    ex05 lista de vetor 02

--- lista-vetor-01/ex05.js
+++ lista-vetor-01/ex05.js
@@ -1,3 +1,10 @@
 let nums = [10, 25, 7, 89, 32, 14];
 
-let maior = nums[]
+let maior = nums[0];
+for(let i = 1; i < nums.length; i++){
+    if(nums[i] > maior){
+        maior = nums[i];
+    }
+}
+
+console.log("maior valor ", maior);
