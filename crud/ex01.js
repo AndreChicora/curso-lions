138ae8aec2c711b48ef4bb499561ef76ec0233d3
andre luiz <andrechicora844@email.com>
2026-03-30 14:43:32 -0300

    ex crud realizada

---
+++ crud/ex01.js
@@ -0,0 +1,21 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+function mostrarMenu() {
+    console.log("/=====================");
+    console.log("SISTEMA ESCOLAR");
+    console.log("=====================");
+    console.log("1 - Cadastrar Aluno");
+    console.log("2 - Listar aluno");
+    console.log("3 - Buscar por ID");
+    console.log("4 - Atualizar Aluno");
+    console.log("5 - Remover Aluno");
+    console.log("6 - Mostrar Alunos Aprovados");
+    console.log("7 - Mostrar Alunos Reprovados");
+    console.log("0 - Sair");
+console.log("=======================");
+}
